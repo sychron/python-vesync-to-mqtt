3246976904e9343302ac7d0fd2573aeac97659ed
sychron <gitgub@sychron.info>
2023-03-25 20:41:46 +0100

    - restructured code to use class except for mqtt client - made pylint a bit happier

--- python-vesync-to-mqtt.py
+++ python-vesync-to-mqtt.py
@@ -4,174 +4,209 @@ from configparser import ConfigParser
 import paho.mqtt.client as mqtt
 from pyvesync import VeSync
 
-# === Config ====================================================
-
 DEBUG = False
+CONTROL_TOPIC = "vesync"
 
-config = ConfigParser()
+class VesyncToMqtt:
+    """Handles VeSync to MQTT translation"""
+    fanbuffer=dict()
+    debug = False
 
-def init_config():
-    global config
-    config.read("python-vesync-to-mqtt.ini")
+    # === Constructor ===============================================
 
-# === VeSync ====================================================
+    def __init__(self, debug = False):
+        self.debug=debug
+        self.init_config()
+        self.init_vesync(debug)
 
-fanbuffer=dict()
-fans = []
+    # === Config ====================================================
 
-def init_vesync(debug = False):
-    """Initializes the vesync connection."""
-    global fans
-    global fanbuffer
-    global config
-    manager = VeSync(
-        config.get('vesync','username'),
-        config.get('vesync','password'),
-        config.get('vesync','timezone'),
-        debug)
-    manager.login()
-    manager.update()
-    fans = manager.fans
-    for fan in fans:
-        fanbuffer[fan.device_name]={"mode":0,"level":0,"status":0, "combined":0}
-        if debug:
-            print (fan)
+    Config = ConfigParser()
+
+    def init_config(self):
+        """Read config file from disk."""
+        self.Config.read("python-vesync-to-mqtt.ini")
+
+    # === VeSync ====================================================
+
+    fans = []
+
+    def init_vesync(self, debug = False):
+        """Initializes the vesync connection."""
+        manager = VeSync(
+            self.Config.get('vesync','username'),
+            self.Config.get('vesync','password'),
+            self.Config.get('vesync','timezone'),
+            debug)
+        manager.login()
+        manager.update()
+        self.fans = manager.fans
+        for fan in self.fans:
+            self.fanbuffer[fan.device_name]={"mode":0,"level":0,"status":0, "combined":0}
+            if debug:
+                print (fan)
+            else:
+                print (fan.device_name)
+
+    def process_fan_info(self):
+        """Detect changes in vesync fan information and publish them"""
+        for fan in self.fans:
+            change = False
+            fan.update()
+            basetopic="vesync/"+fan.device_name+"/"
+            if self.fanbuffer[fan.device_name]["level"] != fan.fan_level:
+                self.fanbuffer[fan.device_name]["level"] = fan.fan_level
+                self.Mqtt_client.publish(basetopic + "level", fan.fan_level, retain=True)
+                change = True
+            if self.fanbuffer[fan.device_name]["status"] != fan.device_status:
+                self.fanbuffer[fan.device_name]["status"] = fan.device_status
+                self.Mqtt_client.publish(basetopic + "status", fan.device_status, retain=True)
+            if self.fanbuffer[fan.device_name]["mode"] != fan.mode:
+                self.fanbuffer[fan.device_name]["mode"] = fan.mode
+                self.Mqtt_client.publish(basetopic + "mode", fan.mode, retain=True)
+                change = True
+            if change:
+                if "manual" == fan.mode:
+                    self.fanbuffer[fan.device_name]["combined"] = fan.fan_level
+                elif "sleep" == fan.mode:
+                    self.fanbuffer[fan.device_name]["combined"] = 0
+                self.Mqtt_client.publish(
+                    basetopic + "combined",
+                    self.fanbuffer[fan.device_name]["combined"],
+                    retain=True)
+
+    def set_combined(self, fan, value):
+        """Interpret combined state and set fan accordingly."""
+        if self.debug:
+            print ("Setting combined.")
+        value = int(value)
+        if (value > 0) and (value < 4):
+            fan.change_fan_speed(value)
+        elif value == 0:
+            fan.sleep_mode()
+            return False
+        return True
+
+    def set_fan_level(self, fan, value):
+        """Check bounds of fan level value and transmit to vesync if ok."""
+        if self.debug:
+            print ("Setting level.")
+        value = int(value)
+        if (value > 0) and (value < 4):
+            fan.change_fan_speed(value)
         else:
-            print (fan.device_name)
+            if self.debug:
+                print ("invalid speed: ", value)
+            return False
+        return True
 
-def process_fan_info():
-    """Detect changes in vesync fan information and publish them"""
-    global fans
-    global fanbuffer
-    for fan in fans:
-        change = False
-        fan.update()
-        basetopic="vesync/"+fan.device_name+"/"
-        if fanbuffer[fan.device_name]["level"] != fan.fan_level:
-            fanbuffer[fan.device_name]["level"] = fan.fan_level
-            client.publish(basetopic + "level", fan.fan_level, retain=True)
-            change = True
-        if fanbuffer[fan.device_name]["status"] != fan.device_status:
-            fanbuffer[fan.device_name]["status"] = fan.device_status
-            client.publish(basetopic + "status", fan.device_status, retain=True)
-        if fanbuffer[fan.device_name]["mode"] != fan.mode:
-            fanbuffer[fan.device_name]["mode"] = fan.mode
-            client.publish(basetopic + "mode", fan.mode, retain=True)
-            change = True
-        if change:
-            if "manual" == fan.mode:
-                fanbuffer[fan.device_name]["combined"] = fan.fan_level
-            elif "sleep" == fan.mode:
-                fanbuffer[fan.device_name]["combined"] = 0
-            client.publish(basetopic + "combined", fanbuffer[fan.device_name]["combined"], retain=True)
+    def set_fan_mode(self, fan, mode):
+        """transmit fan mode to vesync if legal."""
+        if self.debug:
+            print ("Setting mode.")
+        if "sleep " == mode:
+            fan.sleep_mode()
+        elif "manual" == mode:
+            fan.manual_mode()
+        elif "auto" == mode:
+            fan.auto_mode()
+        else:
+            if self.debug:
+                print ("invalid mode: ", mode)
+            return False
+        return True
 
-def setCombined(fan, value):
-    global DEBUG
-    if DEBUG:
-        print ("Setting combined.")
-    value = int(value)
-    if (value > 0) and (value < 4):
-        fan.change_fan_speed(value)
-    elif (value == 0):
-        fan.sleep_mode()
-    return
+    # === MQTT ======================================================
 
-def setFanLevel(fan, value):
-    global DEBUG
-    if DEBUG:
-        print ("Setting level.")
-    value = int(value)
-    if (value > 0) and (value < 4):
-        fan.change_fan_speed(value)
-    else:
-        if DEBUG:
-            print ("invalid speed: ", value)
-    return
+    Mqtt_client = None
 
-def setFanMode(fan, mode):
-    global DEBUG
-    if DEBUG:
-        print ("Setting mode.")
-    if "sleep " == mode:
-        fan.sleep_mode()
-    elif "manual" == mode:
-        fan.manual_mode()
-    elif "auto" == mode:
-        fan.auto_mode()
-    else:
-        if DEBUG:
-            print ("invalid mode: ", mode)
-    return
+    def set_mqtt_client(self, the_client):
+        """Sets local client reference"""
+        self.Mqtt_client = the_client
 
-# === MQTT ======================================================
+    def on_connect(self, client, userdata, flags, rc):
+        """MQTT event handler: connect notification"""
+        print("MQTT Connected, result code "+str(rc))
+        client.subscribe(CONTROL_TOPIC + "/#")
 
-client = mqtt.Client()
+    def on_message(self, client, userdata, msg):
+        """Handle messages received by the client"""
+        topic = msg.topic.split("/")
+        if not CONTROL_TOPIC == topic[0]:
+            return
+        if len(topic) < 4:    # topic long enaugh?
+            if self.debug:
+                print ("topic too short")
+            return
+        if topic[3] != "set": # no setter, no service
+            return
+        the_fan = dict()
+        found = False
+        for fan in self.fans:
+            if fan.device_name==topic[1]:
+                the_fan = fan
+                found = True
+        if not found:
+            if self.debug:
+                print ("device unknown")
+            return
+        payload = msg.payload.decode("utf-8")
+        if self.debug:
+            print ("Addressing fan:", the_fan.device_name)
+        if "combined" == topic[2]:
+            self.set_combined (the_fan, payload)
+        elif "mode" == topic[2]:
+            self.set_fan_mode (the_fan, payload)
+        elif "level" == topic[2]:
+            self.set_fan_level (the_fan, payload)
+        else:
+            if self.debug:
+                print ("invalid control command: ", topic[2])
 
-controlTopic = "vesync"
-RunLoop = True
+# === Setup the handler class ===================================
 
-def on_connect(client, userdata, flags, rc):
-    global controlTopic
-    print("MQTT Connected, result code "+str(rc))
-    client.subscribe(controlTopic + "/#")
+vesync_to_mqtt = VesyncToMqtt(DEBUG)
 
-def on_message(client, userdata, msg):
-    """Handle messages received by the client"""
-    global DEBUG
-    global controlTopic
-    global fanbuffer
-    global fans
-    topic = msg.topic.split("/")
-    if not (topic[0] == controlTopic):
-        return
-    if len(topic) < 4:    # topic long enaugh?
-        if DEBUG:
-            print ("topic too short")
-        return
-    if topic[3] != "set": # no setter, no service
-        return
-    theFan = dict()
-    found = False
-    for fan in fans:
-        if ( fan.device_name==topic[1] ):
-            theFan = fan
-            found = True
-    if not found:
-        if DEBUG:
-            print ("device unknown")
-        return False
-    payload = msg.payload.decode("utf-8")
-    if DEBUG:
-        print ("Addressing fan:", theFan.device_name)
-    if "combined" == topic[2]:
-        setCombined (theFan, payload)
-    elif "mode" == topic[2]:
-        setFanMode (theFan, payload)
-    elif "level" == topic[2]:
-        setFanLevel (theFan, payload)
+# === Setup PAHO MQTT (cannot be inside the class) ==============
 
-def init_mqtt():
-    global config
+def mqtt_on_connect(client, userdata, flags, rc):
+    """PAHO MQTT event handler: connect notification"""
+    vesync_to_mqtt.on_connect(client, userdata, flags, rc)
+
+def mqtt_on_message(client, userdata, msg):
+    """PAHO MQTT event handler: message received"""
+    vesync_to_mqtt.on_message(client, userdata, msg)
+
+def init_mqtt(vesync_connector):
+    """Setup PAHO MQTT client."""
+    client = mqtt.Client()
     client.reinitialise(
-        config.get('mqtt','clientid',
+        vesync_connector.Config.get(
+            'mqtt','clientid',
             fallback="python-vesync-to-mqtt"))
-    client.on_connect = on_connect
-    client.on_message = on_message
+    client.on_connect = mqtt_on_connect
+    client.on_message = mqtt_on_message
     client.username_pw_set(
-        config.get('mqtt','username'),
-        config.get('mqtt','password'))
+        vesync_connector.Config.get('mqtt','username'),
+        vesync_connector.Config.get('mqtt','password'))
     client.connect(
-        config.get('mqtt','brokerip'),
-        config.getint('mqtt','brokerport',fallback=1883),
+        vesync_connector.Config.get('mqtt','brokerip'),
+        vesync_connector.Config.getint('mqtt','brokerport',fallback=1883),
         60)
     client.loop_start()
+    return client
+
+mqtt_client = init_mqtt(vesync_to_mqtt)
+vesync_to_mqtt.set_mqtt_client(mqtt_client)
+
 
 # === Main ======================================================
 
-init_config()
-init_vesync(DEBUG)
-init_mqtt()
+# it's a variable, not a constant
+# pylint: disable=C0103
+run_loop = True
+# pylint: enable=C0103
 
-while RunLoop:
+while run_loop:
     #client.loop(timeout=1.0, max_packets=10)
-    process_fan_info()
+    vesync_to_mqtt.process_fan_info()
